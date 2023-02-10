5632e5017c16b6774378a131baec91a51ea04e85
jordi <jroselln@gmail.com>
2023-02-10 09:38:46 +0100

    pins test

--- pins/pins.R
+++ pins/pins.R
@@ -10,6 +10,24 @@ squirrels <-
   ) %>%
   mutate(squirrels = ifelse(squirrels, "squirrels", "no squirrels"))
 
-board %>% pins::pin_write(squirrels, "squirrels")
+board %>% pins::pin_write(squirrels, "squirrels", type = "rds")
 board %>% pins::pin_list()
 board %>% pins::pin_read("squirrels")
+
+
+
+github_raw <- function(x) paste0("https://raw.githubusercontent.com/", x)
+
+## with a named vector of URLs to specific pins:
+b1 <- board_url(c(
+  files = github_raw("rstudio/pins-r/main/tests/testthat/pin-files/"),
+  rds = github_raw("rstudio/pins-r/main/tests/testthat/pin-rds/"),
+  raw = github_raw("rstudio/pins-r/main/tests/testthat/pin-files/first.txt")
+))
+
+b1 %>% pin_read("squirrels")
+
+board <- board_url(c(
+  squirrels = "https://raw.githubusercontent.com/jrosell/tidytuesday/main/pins/squirrels/"
+))
+board %>% pin_read("squirrels")
