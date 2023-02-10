b17971aad80597d22ae22f8b5f57dcc92b50e9d1
jordi <jroselln@gmail.com>
2023-02-10 09:58:31 +0100

    pins instructions

--- pins/pins.R
+++ pins/pins.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(tidymodels)
 library(pins)
 
-# Create local folder
+# 1. Create a board in the local folder
 board <- pins::board_folder(here::here("pins"))
 squirrels <- 
   read_csv(
@@ -14,7 +14,9 @@ board %>% pins::pin_write(squirrels, "squirrels", type = "rds")
 board %>% pins::pin_list()
 board %>% pins::pin_read("squirrels")
 
-# Read from github
+# 2. Git add, commit, push
+
+# 3. Read a board from github
 github_raw <- function(x) paste0("https://raw.githubusercontent.com/", x)
 board <- board_url(c(
   squirrels = github_raw("jrosell/tidytuesday/main/pins/squirrels/20230210T083753Z-a5873/")
